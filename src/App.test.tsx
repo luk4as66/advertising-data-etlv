3bb5c9b3994d87807146517b99a625f3c8551ea7
luk4as66@gmail.com <Master;_666>
2022-01-19 12:47:15 +0100

    cleanup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,45 +3,12 @@ import { render, screen, waitFor } from "@testing-library/react";
 import HttpRequestMock from "http-request-mock";
 import { Method } from "http-request-mock/dist/types";
 import App from "./App";
-import { CampaignRaw } from "./utils/dataTypes";
-import * as dataUtils from "./utils/dataUtils";
 import { ADVERTISING_DATA_URL } from "./consts";
 
 const mocker = HttpRequestMock.setup();
 
 const mockCsvString =
   "Date,Campaign,Datasource,Click,Impressions\n01.02.2012,Cmp1,Dts1,34,53";
-
-const mockData: ReadonlyArray<CampaignRaw> = [
-  {
-    Date: "03.03.2020",
-    Datasource: "source1",
-    Clicks: 12,
-    Campaign: "campaign1",
-    Impressions: 4,
-  },
-  {
-    Date: "25.02.2022",
-    Datasource: "source3",
-    Clicks: 2,
-    Campaign: "campaign3",
-    Impressions: 1,
-  },
-  {
-    Date: "25.03.2022",
-    Datasource: "source3",
-    Clicks: 32,
-    Campaign: "campaign4",
-    Impressions: 23,
-  },
-  {
-    Date: "01.02.2019",
-    Datasource: "source2",
-    Clicks: 34,
-    Campaign: "campaign2",
-    Impressions: 23,
-  },
-];
 
 describe("Dashboard", () => {
   test("should render progressbar ", async () => {
