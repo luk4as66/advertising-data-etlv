091128b68e9349ced6ef59cb72d528b35391660e
luk4as66@gmail.com <Master;_666>
2022-01-19 15:42:53 +0100

    add proxy to resolve safari strict policies

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 export const SERVER_BASE =
-  "http://adverity-challenge.s3-website-eu-west-1.amazonaws.com";
+  process.env.NODE_ENV === "development"
+    ? ""
+    : "http://adverity-challenge.s3-website-eu-west-1.amazonaws.com";
 export const ADVERTISING_DATA_ENDPOINT = "/DAMKBAoDBwoDBAkOBAYFCw.csv";
 
 export const ADVERTISING_DATA_URL = SERVER_BASE + ADVERTISING_DATA_ENDPOINT;
